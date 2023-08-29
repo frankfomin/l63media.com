92cff0f69408044bec40545dfd62c94265ad7454
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2023-08-29 17:54:43 +0200

    import fix

--- src/components/projectCard/AnimationPopup.tsx
+++ src/components/projectCard/AnimationPopup.tsx
@@ -2,7 +2,6 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
-import { VideoPlayer } from "../VideoPlayer";
 
 type AnimationPopup = {
   children: React.ReactNode;
