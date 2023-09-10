b993c354e4f1f69ea649b727b2858871afe47bed
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2023-09-10 17:07:14 +0200

    ass

--- src/components/videoPlayers/VideoPlayer.tsx
+++ src/components/videoPlayers/VideoPlayer.tsx
@@ -248,22 +248,22 @@ export default function VideoPlayer({ title, vimeoPath }: VideoPlayerProps) {
               muted
               autoPlay
               loop
-              playsInline
               className="w-full h-full absolute opacity-70  rounded-[4rem] aspect-video"
               preload="auto"
               controlsList="nofullscreen"
             />
-              <ReactPlayer
-                ref={(player) => (reactPlayerRef.current = player)}
-                url={`https://vimeo.com/858377870`}
-                playing
-                loop
-                playsInline
-                controls={false}
-                muted={muted}
-                width="100%"
-                height="100%"
-              />
+        
+            <ReactPlayer
+              ref={(player) => (reactPlayerRef.current = player)}
+              url={`https://vimeo.com/858377870`}
+              playing
+              loop
+              controls={false}
+              muted={muted}
+              width="100%"
+              height="100%"
+              playsinline
+            />
           </div>
         </header>
       ) : (
