54be788c6e4d210f1fc79f8ba8034cd80a89189b
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2024-08-15 23:50:15 +0200

    test

--- src/components/ui/MovingLogos.tsx
+++ src/components/ui/MovingLogos.tsx
@@ -3,9 +3,10 @@ import { InfiniteMovingCards } from "./infinite-moving-cars";
 
 export default function MovingLogos() {
   return (
-    <section className="relative flex w-full justify-center bg-secondary">
+    <InfiniteMovingCards />
+
+    /*     <section className="relative flex w-full justify-center bg-secondary">
       <div className="absolute left-1/2 top-1/2 h-[100%] w-[100%] -translate-x-1/2 -translate-y-1/2 transform shadow-[inset_0px_0px_3px_3px_#1d1a1a]"></div>
-      <InfiniteMovingCards />
-    </section>
+    </section> */
   );
 }
